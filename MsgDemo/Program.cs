ea4b211de776df745c9f52339037b26a2bd8ca4a
Gerald-Hub-Town <w15050316701@163.com>
2021-04-14 08:50:20 +0800

    添加项目文件。

---
+++ MsgDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace MsgDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = @"http://push.ijingniu.cn/send?key=da19e954e7c54383aedb13d44f09e311&head=Hello&body=Hello.World";
+
+            var xx = await url.GetAsync();
+
+            Console.WriteLine("发送完成");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
